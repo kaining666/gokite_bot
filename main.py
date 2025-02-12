197b9cffea4fc5e8f62bba7399edc2d8e836b23a
kaining666 <420610383@qq.com>
2025-02-12 22:31:55 +0800

    main

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ from bot import gokit_bot
 from bot_proxy import ProxyChecker
 from wallet import EthereumWallet
 
-def run():
+def register():
     wallet_manager = EthereumWallet()
     wallet_data = wallet_manager.generate_wallet()
     checker = ProxyChecker()
-    try:
-        proxy = checker.get_unique_proxy()
-        bot = gokit_bot(private_key=wallet_data["private_key"], proxy=proxy)
-        bot.connect_wallet()
-        bot.mission_success()
-        bot.get_state()
-        wallet_manager.save_wallet(wallet_data)
-    except Exception as e:
-        print(e)
+    while True:
+        try:
+            proxy = checker.get_unique_proxy()
+            bot = gokit_bot(address=wallet_data["address"],private_key=wallet_data["private_key"], proxy=proxy)
+            bot.connect_wallet()
+            bot.mission_success()
+            bot.get_state()
+            wallet_manager.save_wallet(wallet_data)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
-    run()
+    register()
