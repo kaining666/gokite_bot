ebd104b7eda9f1e9ee2d2e0e0659dcf7048ba850
kaining666 <420610383@qq.com>
2025-02-15 14:11:55 +0800

    main

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from bot import gokit_bot
 from bot_proxy import ProxyChecker
+from utils import DataProviderFromFile
 from wallet import EthereumWallet
+from xAuth import XAuth
+
+xfile = '/Users/ykn/PycharmProjects/gokite_bot/twitter_token.txt'
 
 def register():
     wallet_manager = EthereumWallet()
     wallet_data = wallet_manager.generate_wallet()
     checker = ProxyChecker()
+    provider = DataProviderFromFile(xfile)
     while True:
         try:
             proxy = checker.get_unique_proxy()
+            twitter_token = provider.get_data()
+            xauth = XAuth(auth_token=twitter_token)
             bot = gokit_bot(address=wallet_data["address"],private_key=wallet_data["private_key"], proxy=proxy)
             bot.connect_wallet()
+
             bot.mission_success()
-            bot.get_state()
-            wallet_manager.save_wallet(wallet_data)
+            bot.tel_mission()
+
+            oauth_token = bot.social_link('twitter')
+            verifier = xauth.oauth1(oauth_token)
+
+            ref_code = bot.get_state()
+            wallet_manager.save_wallet(wallet_data,ref_code)
         except Exception as e:
             print(e)
 
