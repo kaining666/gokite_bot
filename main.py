33db856888eadbd16fde43686dddaba50811cfbb
kaining666 <420610383@qq.com>
2025-02-17 23:50:14 +0800

    main

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ from xAuth import XAuth
 import os
 
 xfile = os.path.abspath('twitter_token.txt')
+dfile = os.path.abspath('discord_token.txt')
 
 def register():
     wallet_manager = EthereumWallet()
     wallet_data = wallet_manager.generate_wallet()
     checker = ProxyChecker()
-    provider = DataProviderFromFile(xfile)
+    twitter_data = DataProviderFromFile(xfile)
+    discord_data = DataProviderFromFile(dfile)
     while True:
         try:
             proxy = checker.get_unique_proxy()
-            twitter_token = provider.get_data()
+            twitter_token = twitter_data.get_data()
+            discord_token = discord_data.get_data()
             xauth = XAuth(auth_token=twitter_token,proxy=proxy)
             bot = gokit_bot(address=wallet_data["address"],private_key=wallet_data["private_key"], proxy=proxy)
             bot.connect_wallet()
@@ -30,9 +33,15 @@ def register():
             verifier = xauth.oauth1(oauth_token)
             bot.submit_x_token(social='twitter',oauth_token=oauth_token,oauth_verifier=verifier)
 
+            #连接discord
+            token = bot.social_link('discord')
+            data = bot.submit_discord_token(token, discord_token)
+
             #保存地址，私钥，邀请码
             ref_code = bot.get_state()
             wallet_manager.save_wallet(wallet_data,ref_code)
+
+
         except Exception as e:
             print(e)
 
