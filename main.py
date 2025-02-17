c23a211fd1791a1668029117b5bc313809d51e46
kaining666 <420610383@qq.com>
2025-02-17 10:42:16 +0800

    main

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ def register():
             xauth = XAuth(auth_token=twitter_token)
             bot = gokit_bot(address=wallet_data["address"],private_key=wallet_data["private_key"], proxy=proxy)
             bot.connect_wallet()
-
+            #50分
             bot.mission_success()
+
+            #电报任务
             bot.tel_mission()
 
+            #twitter验证
             oauth_token = bot.social_link('twitter')
             verifier = xauth.oauth1(oauth_token)
+            bot.submit_x_token(social='twitter',oauth_token=oauth_token,oauth_verifier=verifier)
 
+            #保存地址，私钥，邀请码
             ref_code = bot.get_state()
             wallet_manager.save_wallet(wallet_data,ref_code)
         except Exception as e:
